cd6c55a2895b92c814351083571cd3a4bf9fe90c
Mathias Colpaert <colpaert.mathias@gmail.com>
2017-12-04 09:45:51 +0100

    update erc20 txn handler

--- app/modules/crypto/managers/erc20-transaction-handler.js
+++ app/modules/crypto/managers/erc20-transaction-handler.js
@@ -1,11 +1,11 @@
 const logger = require('../../../framework/logger');
 const models = require('../models');
 
-class Erc20TransactionHandler{
-    async handleTransactions(coin, transactions){
+class Erc20TransactionHandler {
+    async handleTransactions(coin, transactions) {
         logger.info(`handling ${transactions.length} ${coin.description} transactions.`);
 
-        for(const txn of transactions){
+        for (const txn of transactions) {
             const existing = await models.Erc20Transaction.findOne({
                 where: {
                     coinId: coin.id,
@@ -15,12 +15,44 @@ class Erc20TransactionHandler{
                 },
             });
 
-            if(existing === null){
-                await models.Erc20Transaction.create(txn);
-            }else{
-                existing.isRemoved = txn.isRemoved;
-                existing.save();
+            if (existing === null) {
+                await this.createTransaction(txn);
+            } else {
+                await this.updateTransaction(txn, coin);
             }
+        }
+    }
+
+    async createTransaction(txn) {
+        try{
+            await models.Erc20Transaction.create(txn);
+        }catch(err){
+            logger.error('could not insert transaction!');
+            logger.error(JSON.stringify(txn));
+            throw err;
+        }
+    }
+
+    async updateTransaction(txn, coin){
+        const data = {
+            isRemoved: txn.isRemoved,
+        };
+        const options = {
+            fields: ['isRemoved'],
+            where: {
+                coinId: coin.id,
+                blockNumber: txn.blockNumber,
+                transactionHash: txn.transactionHash,
+                logIndex: txn.logIndex,
+            },
+        };
+
+        try{
+            await models.Erc20Transaction.update(data, options);
+        }catch(err){
+            logger.error('could not update transaction!');
+            logger.error(JSON.stringify(txn));
+            throw err;
         }
     }
 }
