64f9bf44df99ff821472b460468886ba5425a3a4
Mathias Colpaert <colpaert.mathias@gmail.com>
2017-12-20 16:14:16 +0100

    sync prices

--- app/modules/crypto/controllers/price-controller.js
+++ app/modules/crypto/controllers/price-controller.js
@@ -21,6 +21,61 @@ class PriceController{
             return;
         }
 
+        // const coinId = req.query.coinId;
+        // const currencyId = req.query.currencyId;
+        //const from = req.query.from;
+        //const to = req.query.to;
+        const jobId = req.query.jobId;
+
+        const fromDate = new Date(2016, 1, 1);
+        const toDate = new Date();
+        // const coin = await models.Coin.findOne({
+        //     where: {
+        //         id: coinId,
+        //     },
+        // });
+
+        // const currency = await models.Currency.findOne({
+        //     where: {
+        //         id: currencyId,
+        //     },
+        // });
+
+        logger.verbose(`completing prices from ${fromDate} untill ${toDate}`);
+        logger.verbose(`job id = ${jobId}`);
+
+        res.sendStatus(HttpStatus.ACCEPTED);
+
+        let date = new Date(fromDate.getFullYear(), fromDate.getMonth(), fromDate.getDay(), 12, 0, 0);
+        const dates = [];
+        while(date < toDate) {
+            dates.push(date);
+            date = new Date(date.getTime() + 24*60*60*1000);
+        }
+
+        const currencies = await models.Currency.findAll();
+        const coins = await models.Coin.findAll({
+            where: {
+                isActive: true,
+            },
+        });
+
+        await this.priceManager.getPricesForCurrencies(dates, coins, currencies);
+
+        if(jobId !== undefined && jobId != null){
+            logger.verbose(`calling done on job ${jobId}`);
+            await this.jobManager.setDone(req.userId, jobId);
+        }else{
+            logger.verbose('no jobId passed in query params');
+        }
+    }
+
+    /*async create(req, res){
+        if(!req.isAdmin){
+            res.sendStatus(HttpStatus.UNAUTHORIZED);
+            return;
+        }
+
         res.sendStatus(HttpStatus.ACCEPTED);
 /*
         const date = new Date();
@@ -47,8 +102,8 @@ class PriceController{
             await this.jobManager.setDone(req.userId, jobId);
         }else{
             logger.verbose('no jobId passed in query params');
-        }*/
-    }
+        }
+    }*/
 }
 
 module.exports = PriceController;
